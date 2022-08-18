098ca1b71ed8b4132bdd849ca94240c13bb9dcaa
chebok <vovakuzmin@gmail.com>
2022-08-18 13:38:08 +0300

    fix 1

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 /* eslint-disable import/extensions */
 import Validator from './src/Validator.js';
 
-const v = new Validator();
-
-const schema = v.string();
-export default schema;
+export default Validator;
